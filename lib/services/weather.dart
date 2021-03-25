990f70e8c811069009187d8db8f6597f05c7f49d
ectaw6d <Andrew.Palet@McKesson.com>
2021-03-25 11:53:06 -0500

    last pieces to get the location and check to see if the location isn't null

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,15 @@ const apiKey = '95bc2a8a31c8d85b9a4fa22aa454856b';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey';
+    print(url);
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentPosition();
